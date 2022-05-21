8b9d5c8eb388ea272598613687e4b00c4b304771
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-05-21 19:43:47 +0300

    01-gallery done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,49 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem(item) {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+};
+
+const createGallery = galleryItems.map(item => createGalleryItem(item)).join('');
+
+
+gallery.insertAdjacentHTML('beforeend', createGallery);
+
+gallery.addEventListener('click', handlerOpenImageClick);
+
+function handlerOpenImageClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') { return };
+    console.log(evt.target.dataset.source);
+    
+    
+    const instance = basicLightbox.create(`
+		<img width="1000" height="800" src="${evt.target.dataset.source}">
+	`);
+    
+    instance.show();
+
+
+    window.addEventListener('keydown', handleEscapeClick);
+
+    function handleEscapeClick(evt) {
+        console.log(evt.code);// Escape
+        if (evt.code === 'Escape') {            
+            instance.close();
+      }
+  }
+};
